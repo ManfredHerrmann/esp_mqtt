f9813e8b1e46ed9dbe236585de11c774346c69cb
Tuan PM <tuanpm@live.com>
2014-12-26 15:47:16 +0700

    change default configuration

--- include/user_config.h
+++ include/user_config.h
@@ -5,7 +5,7 @@
 #define CFG_HOLDER	0x00FF55A1
 #define CFG_LOCATION	0x3C
 
-#define MQTT_HOST	"192.168.11.117"
+#define MQTT_HOST	"192.168.11.117" //or "mqtt.domain.com"
 #define MQTT_PORT	2222
 #define MQTT_BUF_SIZE	1024
 
@@ -21,11 +21,11 @@
 #define KEY "39cdfe29a1863489e788"
 
 #define AP_SSID "DVES_%08X"
-#define AP_PASS "dongvietco"
+#define AP_PASS "dves"
 #define AP_TYPE AUTH_OPEN
 
 #define STA_SSID "DVES_HOME"
-#define STA_PASS "dongvietdves"
+#define STA_PASS "dves123456"
 #define STA_TYPE AUTH_WPA2_PSK
 
 #define MQTT_RECONNECT_TIMEOUT 5
